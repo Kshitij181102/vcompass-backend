c58626a8f5f77d530b50c85011f261eb769ba223
Raj <raj@gmail.com>
2025-05-23 17:38:42 +0530

    Fixed everything

--- utils/getConnection.js
+++ utils/getConnection.js
@@ -4,7 +4,7 @@ const mongoose = require("mongoose");
 const db1Uri = process.env.MONGO_URI1;
 let db1Connection = null;
 
-// Singleton connection for db1
+// Singleton connection for db1 creating user
 const connectDb1 = () => {
   if (!db1Connection) {
     db1Connection = mongoose.createConnection(db1Uri, {
@@ -16,24 +16,25 @@ const connectDb1 = () => {
   return db1Connection;
 };
 
-// For db2, keep it as-is
+// For db2, keep it as-is Booking database
 const db2Uri = process.env.MONGO_URI2;
 const connectDb2 = () => {
   return mongoose.createConnection(db2Uri, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
   });
+  
 };
 
 const db3Uri = process.env.MONGO_URI3;
-
+// used for the mentor
 const connectDb3 = () => {
   return mongoose.createConnection(db3Uri, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
   });
 };
-
+//useed for the news
 const db4Uri = process.env.MONGO_URI4;
 const connectDb4 = () => {
   return mongoose.createConnection(db4Uri, {
@@ -41,6 +42,7 @@ const connectDb4 = () => {
     useUnifiedTopology: true,
   });
 };
+// used for the poster
 const db5Uri = process.env.MONGO_URI5;
 const connectDb5 = () => {
   return mongoose.createConnection(db5Uri, {
